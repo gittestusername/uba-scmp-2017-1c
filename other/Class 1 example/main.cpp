034605cab5ac8841778029055b02e4b1cf183bf0
martin <venturamartin90@gmail.com>
2017-04-09 20:40:25 -0300

    Merging notebook and desktop files

---
+++ other/Class 1 example/main.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main(){
+
+	//Construct system:
+
+	vector<double> curr;
+	vector<double> last;
+
+
+	int n = 400;
+	int steps = 100;
+	double r = 0.1;
+
+	for(int i = 0; i < n; ++i){
+		if(i == n-1){
+			last.push_back(0);
+			curr.push_back(0);
+		}else{
+			last.push_back(i);
+			curr.push_back(i);	
+		}
+
+	}
+
+	//Simulation:
+
+	for (int i = 0; i < steps; ++i)
+	{
+		for (int i = 1; i < n-1; ++i)
+		{
+			curr[i] = (1.0 - 2.0*r)*last[i] + r*(last[i-1] +last[i+1])/2;
+		}
+
+
+		for (int i = 1; i < n-1; ++i)
+		{
+			last[i] = curr[i];
+		}
+
+
+		//Print:
+		cout << "[";
+		for (int i = 0; i < n; ++i)
+		{
+			cout << curr[i] << ",";
+		}
+		cout << "],";
+	}
+
+
+
+}
