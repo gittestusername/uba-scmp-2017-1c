59e47693c68f79b6045c1dd679013c3dbe5cf90d
martin <venturamartin90@gmail.com>
2017-06-21 13:41:51 -0300

    using external fixed point

--- guide-3/ej6/main.cpp
+++ guide-3/ej6/main.cpp
@@ -23,7 +23,7 @@ void printMat(mat2 m) {
 
 /* EJERCICIO 6 DE PRACTICA 3
     Donde x, y ∈ [0; 2] y t > 0 son las variables espacial y temporal respectivamente. u(x, y, t) y v(x, y, t) son los
-    componentes x-y de la velocidad del fluido dependiente del espacio y el tiempo. Se desea resolver el Liddriven 
+    componentes x-y de la velocidad del fluido dependiente del espacio y el tiempo. Se desea resolver el Liddriven
     cavity problem en el cual a un recipiente (2D) que contiene un determinado fluido se le desliza su tapa
     hacia uno de los lados, provocando cambios en el movimiento y la presi ́on del fluido. Para resolver el problema
     se cuenta con los siguiente datos: ν = 0.1. ρ=1. Condiciones iniciales u, v, p = 0 en todos lados. Condiciones
@@ -35,12 +35,12 @@ int main() {
 
     long double xMax = 2.0;
     long double yMax = 2.0;
-    long double tMax = 1.0;
+    long double tMax = 0.1;
     long double nu = 0.1; //viscosidad
-    long double rho = 1;   //densidad
-    long double dx = 0.2;
-    long double dy = 0.2;
-    long double dt = 0.005;
+    long double rho = 1.0;   //densidad
+    long double dx = 1.0/20.0;
+    long double dy = 1.0/20.0;
+    long double dt = 0.001;
     int nX = round(xMax / dx) + 1;
     int nY = round(yMax / dy) + 1;
     int nT = round(tMax / dt) + 1;
@@ -56,23 +56,21 @@ int main() {
     mat2 U2(nX, vector<long double>(nY));
     mat2 V2(nX, vector<long double>(nY));
     mat2 P2(nX, vector<long double>(nY));
-    mat2 U3(nX, vector<long double>(nY));
-    mat2 V3(nX, vector<long double>(nY));
-    mat2 P3(nX, vector<long double>(nY));
+
+    mat2 U2_old(nX, vector<long double>(nY));
+    mat2 V2_old(nX, vector<long double>(nY));
+    mat2 P2_old(nX, vector<long double>(nY));
 
     for (int i = 0; i < nX; ++i) {
-        fill(U0[i].begin(), U0[i].end(), 0);
-        fill(V0[i].begin(), V0[i].end(), 0);
-        fill(P0[i].begin(), P0[i].end(), 0);
-        fill(U1[i].begin(), U1[i].end(), 0);
-        fill(V1[i].begin(), V1[i].end(), 0);
-        fill(P1[i].begin(), P1[i].end(), 0);
-        fill(U2[i].begin(), U2[i].end(), 0);
-        fill(V2[i].begin(), V2[i].end(), 0);
-        fill(P2[i].begin(), P2[i].end(), 0);
-        fill(U3[i].begin(), U3[i].end(), 0);
-        fill(V3[i].begin(), V3[i].end(), 0);
-        fill(P3[i].begin(), P3[i].end(), 0);
+        fill(U0[i].begin(), U0[i].end(), 0.0);
+        fill(V0[i].begin(), V0[i].end(), 0.0);
+        fill(P0[i].begin(), P0[i].end(), 0.0);
+        fill(U1[i].begin(), U1[i].end(), 0.0);
+        fill(V1[i].begin(), V1[i].end(), 0.0);
+        fill(P1[i].begin(), P1[i].end(), 0.0);
+        fill(U2[i].begin(), U2[i].end(), 0.0);
+        fill(V2[i].begin(), V2[i].end(), 0.0);
+        fill(P2[i].begin(), P2[i].end(), 0.0);
     }
 
     //condicion borde u=1 en y=2
@@ -80,70 +78,93 @@ int main() {
         U0[i][nY - 1] = 1.0;
         U1[i][nY - 1] = 1.0;
         U2[i][nY - 1] = 1.0;
-        U3[i][nY - 1] = 1.0;
     }
 
     for (long double t = 0.0; t < tMax; t = t + dt) {
-         printMat(U0);
-         printMat(V0);
+        cout << "TEMPO:  " << t/dt << endl<< endl<< endl ;
+        if(isnan(U1[3][3])) {
+            exit(EXIT_FAILURE);
+        }        
+        printMat(U0);
+        printMat(V0);
         // printMat(P0);
 
-        for (int i = 1; i < nX - 1; ++i) {
-            for (int j = 1; j < nY - 1; ++j) { //las condiciones borde en Y=2 se respetan aca
-                for (int k = 0; true; ++k) {
-                    long double oldU = U3[i][j];
-                    long double oldV = V3[i][j];
+        for (int c = 0; c < nX; ++c) {
+            P0[c][0] = P0[c][1];
+            P1[c][0] = P1[c][1];
+            P2[c][0] = P2[c][1];
 
-                    long double U1x = (U1[i + 1][j] - U1[i - 1][j]) / (2* dx);
-                    long double U2x = (U2[i + 1][j] - U2[i - 1][j]) / (2* dx);
-                    long double U1y = (U1[i][j + 1] - U1[i][j - 1]) / (2* dy);
-                    long double U2y = (U2[i][j + 1] - U2[i][j - 1]) / (2* dy);
-                    long double U1xx = (U1[i + 1][j] - 2* U1[i][j] + U1[i - 1][j]) / (dx* dx);
-                    long double U2xx = (U2[i + 1][j] - 2* U2[i][j] + U2[i - 1][j]) / (dx* dx);
-                    long double U1yy = (U1[i][j + 1] - 2* U1[i][j] + U1[i][j - 1]) / (dy* dy);
-                    long double U2yy = (U2[i][j + 1] - 2* U2[i][j] + U2[i][j - 1]) / (dy* dy);
-                    long double P1x = (P1[i + 1][j] - P1[i - 1][j]) / (2* dx);
-                    long double P2x = (P2[i + 1][j] - P2[i - 1][j]) / (2* dx);
-                    long double P1y = (P1[i][j + 1] - P1[i][j - 1]) / (2* dy);
-                    long double P2y = (P2[i][j + 1] - P2[i][j - 1]) / (2* dy);
-                    long double V1x = (V1[i + 1][j] - V1[i - 1][j]) / (2* dx);
-                    long double V2x = (V2[i + 1][j] - V2[i - 1][j]) / (2* dx);
-                    long double V1y = (V1[i][j + 1] - V1[i][j - 1]) / (2* dy);
-                    long double V2y = (V2[i][j + 1] - V2[i][j - 1]) / (2* dy);
-                    long double V1xx = (V1[i + 1][j] - 2* V1[i][j] + V1[i - 1][j]) / (dx* dx);
-                    long double V2xx = (V2[i + 1][j] - 2* V2[i][j] + V2[i - 1][j]) / (dx* dx);
-                    long double V1yy = (V1[i][j + 1] - 2* V1[i][j] + V1[i][j - 1]) / (dy* dy);
-                    long double V2yy = (V2[i][j + 1] - 2* V2[i][j] + V2[i][j - 1]) / (dy* dy);
-                    long double P1xx = (P1[i + 1][j] - 2* P1[i][j] + P1[i - 1][j]) / (dx* dx);
-                    long double P1yy = (P1[i][j + 1] - 2* P1[i][j] + P1[i][j - 1]) / (dy* dy);
-                    long double P2yy = (P2[i][j + 1] - 2* P2[i][j] + P2[i][j - 1]) / (dy* dy);
+        }
+
+        for (int c = 0; c < nY; ++c) {
+            P0[nX - 1][c] = P0[nX - 2][c];
+            P1[nX - 1][c] = P1[nX - 2][c];
+            P2[nX - 1][c] = P2[nX - 2][c];
+
+            P0[0][c] = P0[1][c];
+            P1[0][c] = P1[1][c];
+            P2[0][c] = P2[1][c];
+        }
+
+        for (int k = 0; k < 1000; ++k) {
+
+            //U2_old = U2;
+            //V2_old = V2;
+            //P2_old = P2;
+
+            for (int i = 1; i < nX - 1; ++i) {
+                for (int j = 1; j < nY - 1; ++j) { //las condiciones borde en Y=2 se respetan aca
+
+                    //long double oldU = U2[i][j];
+                    //long double oldV = V2[i][j];
+
+                    long double U1x = (U1[i +   1][  j] - U1[i - 1][j]) / (2.0 * dx);
+                    long double U2x = (U2[i + 1][j] - U2[i - 1][j]) / (2.0 * dx);
+                    long double U1y = (U1[i][j + 1] - U1[i][j - 1]) / (2.0 * dy);
+                    long double U2y = (U2[i][j + 1] - U2[i][j - 1]) / (2.0 * dy);
+                    long double U1xx = (U1[i + 1][j] - 2.0 * U1[i][j] + U1[i - 1][j]) / (dx * dx);
+                    long double U2xx = (U2[i + 1][j] - 2.0 * U2[i][j] + U2[i - 1][j]) / (dx * dx);
+                    long double U1yy = (U1[i][j + 1] - 2.0 * U1[i][j] + U1[i][j - 1]) / (dy * dy);
+                    long double U2yy = (U2[i][j + 1] - 2.0 * U2[i][j] + U2[i][j - 1]) / (dy * dy);
+                    long double P1x = (P1[i + 1][j] - P1[i - 1][j]) / (2.0 * dx);
+                    long double P2x = (P2[i + 1][j] - P2[i - 1][j]) / (2.0 * dx);
+                    long double P1y = (P1[i][j + 1] - P1[i][j - 1]) / (2.0 * dy);
+                    long double P2y = (P2[i][j + 1] - P2[i][j - 1]) / (2.0 * dy);
+                    long double V1x = (V1[i + 1][j] - V1[i - 1][j]) / (2.0 * dx);
+                    long double V2x = (V2[i + 1][j] - V2[i - 1][j]) / (2.0 * dx);
+                    long double V1y = (V1[i][j + 1] - V1[i][j - 1]) / (2.0 * dy);
+                    long double V2y = (V2[i][j + 1] - V2[i][j - 1]) / (2.0 * dy);
+                    long double V1xx = (V1[i + 1][j] - 2.0 * V1[i][j] + V1[i - 1][j]) / (dx * dx);
+                    long double V2xx = (V2[i + 1][j] - 2.0 * V2[i][j] + V2[i - 1][j]) / (dx * dx);
+                    long double V1yy = (V1[i][j + 1] - 2.0 * V1[i][j] + V1[i][j - 1]) / (dy * dy);
+                    long double V2yy = (V2[i][j + 1] - 2.0 * V2[i][j] + V2[i][j - 1]) / (dy * dy);
+                    long double P1xx = (P1[i + 1][j] - 2.0 * P1[i][j] + P1[i - 1][j]) / (dx * dx);
+                    long double P1yy = (P1[i][j + 1] - 2.0 * P1[i][j] + P1[i][j - 1]) / (dy * dy);
+                    long double P2yy = (P2[i][j + 1] - 2.0 * P2[i][j] + P2[i][j - 1]) / (dy * dy);
 
                     //TODO: chequear despejes de ecuación 3
-                    U3[i][j] = U0[i][j] + 2* dt* (-U1[i][j]* (al* U1x  + (1 - al)* U2x ) - V1[i][j]* (al* U1y + (1 - al)* U2y)
-                                                   - (1 / rho)* (al* P1x + (1 - al)* P2x) + nu* (al* U1xx + (1 - al)* U2xx + al* U1yy + (1 - al)* U2yy));
-                    V3[i][j] = V0[i][j] + 2* dt* (-U1[i][j]* (al* V1x  + (1 - al)* V2x ) - V1[i][j]* (al* V1y + (1 - al)* V2y)
-                                                   - (1 / rho)* (al* P1y + (1 - al)* P2y) + nu* (al* V1xx + (1 - al)* V2xx + al* V1yy + (1 - al)* V2yy));
-                    P3[i][j] = (P2[i - 1][j] / 2)  + (P2[i + 1][j] / 2) + (dx* dx) / (2* al - 2)* (-al* P1xx - al* P1yy - (1 - al)* P2yy
-                              - rho*((-1/dt) * (U1x + V1y) + pow((al* U1x + (1 - al)* U2x), 2) + 2* (al* U1y + (1 - al)* U2y)* (al* V1x + (1 - al)* V2x) + pow((al* V1y + (1 - al)* V2y), 2)));
+                    U2[i][j] = U0[i][j] + 2.0 * dt * (-U1[i][j] * (al * U1x  + (1 - al) * U2x ) - V1[i][j] * (al * U1y + (1.0 - al) * U2y)
+                                                      - (1.0 / rho) * (al * P1x + (1 - al) * P2x) + nu * (al * U1xx + (1.0 - al) * U2xx + al * U1yy + (1.0 - al) * U2yy));
+                    V2[i][j] = V0[i][j] + 2.0 * dt * (-U1[i][j] * (al * V1x  + (1 - al) * V2x ) - V1[i][j] * (al * V1y + (1.0 - al) * V2y)
+                                                      - (1.0 / rho) * (al * P1y + (1 - al) * P2y) + nu * (al * V1xx + (1.0 - al) * V2xx + al * V1yy + (1.0 - al) * V2yy));
+                    //P2[i][j] = (P2[i - 1][j] / 2.0)  + (P2[i + 1][j] / 2.0) + (dx * dx) / (2.0 * al - 2.0) * (-al * P1xx - al * P1yy - (1.0 - al) * P2yy
+                    //           - rho * ((-1.0 / dt) * (U1x + V1y) + pow((al * U1x + (1.0 - al) * U2x), 2) + 2.0 * (al * U1y + (1.0 - al) * U2y) * (al * V1x + (1.0 - al) * V2x) + pow((al * V1y + (1 - al) * V2y), 2)));
 
-                    //P3[i][j] = (P2[i - 1][j] / 2)  + (P2[i + 1][j] / 2) + (dx* dx) / (2* al - 2)* (-al* P1xx - al* P1yy - (1 - al)* P2yy
-                    //          - rho*( pow((al* U1x + (1 - al)* U2x), 2) + 2* (al* U1y + (1 - al)* U2y)* (al* V1x + (1 - al)* V2x) + pow((al* V1y + (1 - al)* V2y), 2)));
+                    P2[i][j] = (P2[i - 1][j] / 2)  + (P2[i + 1][j] / 2) + (dx * dx) / (2 * al - 2) * (-al * P1xx - al * P1yy - (1 - al) * P2yy
+                              - rho * ( pow((al * U1x + (1 - al) * U2x), 2) + 2 * (al * U1y + (1 - al) * U2y) * (al * V1x + (1 - al) * V2x) + pow((al * V1y + (1 - al) * V2y), 2)));
 
-                    long double diff = sqrt(pow(U3[i][j] - oldU, 2) + pow(V3[i][j] - oldV, 2));
+                    //long double diff = sqrt(pow(U2[i][j] - oldU, 2) + pow(V2[i][j] - oldV, 2));
                     //cout << diff << endl;
-                    if (diff < 0.001 && k > 5){
-                        break;
-                    } else if (k > 50) {
-                        //cerr << "ERROR: unstable." << endl;
-                        break;
-                    }
+                    // if (diff < 0.001 && k > 5) {
+                    //     break;
+                    // } else if (k > 50) {
+                    //     //cerr << "ERROR: unstable." << endl;
+                    //     break;
+                    // }
                 }
             }
         }
 
-        U2 = U3;
-        V2 = V3;
-        P2 = P3;
 
         U0 = U1;
         U1 = U2;
