d2332182c750759e50b79b7e4d5cb8ad17058901
Martin <venturamartin90@gmail.com>
2017-07-17 11:04:28 -0300

    parameters change for testing

--- final-work/parameters.h
+++ final-work/parameters.h
@@ -1,6 +1,6 @@
 
-long double xMax = 65.0;
-long double yMax = 65.0;
+long double xMax = 60.0;
+long double yMax = 60.0;
 long double tMax = 20.0;
 long double nu = 0.1; //viscosidad. Era 0.1.
 long double rho = 1.0;  //densidad
