621d60ebd4ad5a9f60eb28e5c330f43536e2683f
Martin <venturamartin90@gmail.com>
2017-07-23 07:43:46 -0300

    parameter change due to testing

--- final-work/parameters.h
+++ final-work/parameters.h
@@ -1,8 +1,8 @@
 using namespace std;
 
-long double xMax = 80.0;
-long double yMax = 80.0;
-long double tMax = 1.50;
+long double xMax = 150.0;
+long double yMax = 150.0;
+long double tMax = 2.0;
 long double nu = 0.01; //viscosidad. Era 0.1.
 long double rho = 1.0;  //densidad
 long double dx = (1.0 / 20.0);
